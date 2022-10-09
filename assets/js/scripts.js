29ce5f52fd296b16ff9a48f71fc3aecaf9fea26c
Insaf Pilakkal <insafpilakkal@gmail.com>
2022-10-09 15:18:49 +0400

    animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -42,10 +42,10 @@ var swiper = new Swiper('.Swiper-testimonial-slider', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-	// autoplay: {
-	// 	delay: 3000,
-	// 	disableOnInteraction: false,
-	// },
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
 	pagination: {
 		el: '.swiper-pagination',
 		type: 'bullets',
@@ -75,4 +75,52 @@ var swiper = new Swiper('.Swiper-testimonial-slider', {
 			spaceBetween: 40,
 		},
 	},
+})
+
+$(window).scroll(function () {
+	var sticky = $('.header'),
+		scroll = $(window).scrollTop()
+
+	if (scroll >= 200) {
+		sticky.addClass('fixed-header')
+	} else {
+		sticky.removeClass('fixed-header')
+	}
+})
+
+document.addEventListener('DOMContentLoaded', function (event) {
+	let offset = 50
+	let circleContainer = document.querySelector('.circle-container')
+	let circlePath = document.querySelector('.circle-container path')
+	let pathLength = circlePath.getTotalLength()
+	circlePath.style.transition = circlePath.style.WebkitTransition = 'none'
+	circlePath.style.strokeDasharray = pathLength
+	circlePath.style.strokeDashoffset = pathLength
+	circlePath.getBoundingClientRect()
+	circlePath.style.transition = circlePath.style.WebkitTransition = 'stroke-dashoffset 10ms linear'
+
+	let updateLoader = () => {
+		let scrollTop = window.scrollY
+		let docHeight = document.body.offsetHeight
+		let winHeight = window.innerHeight
+		let height = docHeight - winHeight
+		let progress = pathLength - (scrollTop * pathLength) / height
+		circlePath.style.strokeDashoffset = progress
+
+		if (scrollTop > offset) {
+			circleContainer.classList.add('active')
+		} else {
+			circleContainer.classList.remove('active')
+		}
+	}
+
+	circleContainer.onclick = function () {
+		window.scrollTo({ top: 0, behavior: 'smooth' })
+	}
+
+	window.onscroll = () => {
+		updateLoader()
+	}
+
+	updateLoader()
 })
