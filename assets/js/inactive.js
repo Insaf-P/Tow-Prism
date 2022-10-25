7a25542bd73efbec22db595e4e8ee34ea74ea239
forever-RABS <49386088+forever-RABS@users.noreply.github.com>
2022-10-25 19:01:02 +0530

    refactor: Hided Our Clientele, Testimonials sections from ui || feat: added toggler js to hide ui

---
+++ assets/js/inactive.js
@@ -0,0 +1,7 @@
+var ourClientDiv = document.getElementById("our-clientele-section");
+var testimonial = document.getElementById("testimonial-section")
+
+
+ourClientDiv.style.display = "none";
+testimonial.style.display = "none";
+
