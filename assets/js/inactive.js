a801752b2c5c39c9a1cd0b02480d6ff9ae7b9c2e
forever-RABS <49386088+forever-RABS@users.noreply.github.com>
2022-10-26 13:36:16 +0530

    chore: css changes

--- assets/js/inactive.js
+++ assets/js/inactive.js
@@ -1,7 +1,9 @@
 var ourClientDiv = document.getElementById("our-clientele-section");
-var testimonial = document.getElementById("testimonial-section")
+var testimonial = document.getElementById("testimonial-section");
+var Subscribe = document.getElementById("Subscribe");
 
 
 ourClientDiv.style.display = "none";
 testimonial.style.display = "none";
+Subscribe.style.display = "none";
 
